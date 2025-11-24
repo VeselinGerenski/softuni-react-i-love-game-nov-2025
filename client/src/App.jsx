ac6df1ffeee8f95b86302859b6f66ea0da6c1021
VeselinGerenski <veselingerenski@gmail.com>
2025-11-24 11:19:19 +0200

    Add login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,12 +8,19 @@ import GameCreate from "./components/game-create/GameCreate.jsx";
 import GameEdit from "./components/game-edit/GameEdit.jsx";
 import Register from "./components/register/Register.jsx";
 import Login from "./components/login/Login.jsx";
-
+import { useState } from "react";
 
 function App() {
+    const [user, setUser] = useState(null);
+
+    const authHandler = (email) => {
+        setUser({
+            email,
+        });
+    }
     return (
         <>
-            <Header />
+            <Header user={user} />
 
             <Routes>
                 <Route path="/" element={<Home />} />
@@ -21,8 +28,8 @@ function App() {
                 <Route path="/games/:gameId/details" element={<Details />} />
                 <Route path="/games/create" element={<GameCreate />} />
                 <Route path="/games/:gameId/edit" element={<GameEdit />} />
-                <Route path="/register" element={<Register />} />
-
+                <Route path="/register" element={<Register user={user} onRegister={authHandler} />} />
+                <Route path="/login" element={<Login onLogin={authHandler} />} />
             </Routes>
 
             <Footer />
