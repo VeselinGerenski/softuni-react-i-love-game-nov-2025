bb64e0cd5f804f4edfd246052f19c2881102eb15
VeselinGerenski <veselingerenski@gmail.com>
2025-11-24 12:24:31 +0200

    Update header with user functionality

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -4,25 +4,30 @@ export default function Header({
     user,
 }) {
     console.log(user?.email);
-    
+
     return (
         <header>
             {/* Navigation */}
             <nav>
-                <Link className="home" to="/"> <img src="./images/logo.png" alt="logo"/></Link>
+                <Link className="home" to="/"> <img src="./images/logo.png" alt="logo" /></Link>
                 <Link to="/games">Catalog</Link>
 
-                {/* Logged-in users */}
-                <div id="user">
-                    <Link to="/games/create">Add Game</Link>
-                    <Link to="/logout">Logout</Link>
-                </div>
+              
+                {user ?
+                //   {/* Logged-in users */}
+                    <div id="user">
+                        <Link to="/games/create">Add Game</Link>
+                        <Link to="/logout">Logout</Link>
+                    </div>
+                    :
+                        //  {/* Guest users */}
+                    <div id="guest">
+                        <Link to="/login">Login</Link>
+                        <Link to="/register">Register</Link>
+                    </div>}
 
-                {/* Guest users */}
-                <div id="guest">
-                    <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
-                </div>
+           
+
             </nav>
         </header>
     )
