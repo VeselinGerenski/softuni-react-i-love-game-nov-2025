6be58cca6b240e091d70e77fa335c098097121ef
VeselinGerenski <veselingerenski@gmail.com>
2025-11-28 16:32:24 +0200

    Update Logout.jsx

--- client/src/components/logout/Logout.jsx
+++ client/src/components/logout/Logout.jsx
@@ -1,9 +1,13 @@
+import { useContext, useEffect } from "react"
 import { Navigate } from "react-router"
+import UserContext from "../../contexts/userContext.js"
 
-export default function Logout({
-    onLogout,
-}) {
-    onLogout()
+export default function Logout() {
+    const { logoutHandler } = useContext(UserContext)
+
+    useEffect(() => {
+        logoutHandler();
+    }, [logoutHandler]);
 
     return (
         <Navigate to='/' />
